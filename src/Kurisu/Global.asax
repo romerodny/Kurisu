fc5eb253b843705410d3d3c931bb591e126fa53f
romerodny <romerodny@gmail.com>
2018-05-22 22:54:13 -0700

    Initial commit

---
+++ src/Kurisu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kurisu.WebApiApplication" Language="C#" %>
---
+++ src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=kurisu-Web-Deploy.pubxml -p:Password=PzTCjbNoZkxftrNr1r9d7yTlPnhhS1iYoR9q3kSKTQP2A2RSsNbRXWFozkeB
+
